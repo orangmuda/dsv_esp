462d5629c8cae5038b4daeb660f3e4b0bf41aa6f
oxctdev <91846073+oxctdev@users.noreply.github.com>
2021-10-14 14:10:53 +0700

    Create vehicle_counting.py

---
+++ vehicle_counting.py
@@ -0,0 +1,25 @@
+#----------------------------------------------
+#--- Author         : Wahyudi
+#--- Mail           : andridev_@wearehackerone.com
+#--- Date           : 14th October's 2021
+#----------------------------------------------
+
+# Imports
+import tensorflow as tf
+
+# Object detection imports
+from utils import backbone
+from api import object_counting_api
+
+input_video = "./input_images_and_videos/vehicle_survaillance.mp4"
+
+# By default I use an "SSD with Mobilenet" model here. See the detection model zoo (https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+detection_graph, category_index = backbone.set_model('ssd_mobilenet_v1_coco_2018_01_28', 'mscoco_label_map.pbtxt')
+
+is_color_recognition_enabled = True # set it to true for enabling the color prediction for the detected objects
+roi = 237 # roi line position
+deviation = 4.5 # the constant that represents the object counting area
+custom_object_name = 'Vehicle' # set it to your custom object name
+targeted_objects_name = "car, truck"
+
+object_counting_api.cumulative_object_counting_y_axis(input_video, detection_graph, category_index, is_color_recognition_enabled, roi, deviation, custom_object_name, targeted_objects_name) # counting all the objects
